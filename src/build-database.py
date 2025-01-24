0efb0b51890a9cde3d1cdf33e4fa865a88ce89da
Tom <tom.decoutere@gmail.com>
2025-01-24 11:54:28 +0100

    add video db build script

---
+++ src/build-database.py
@@ -0,0 +1,22 @@
+from yt_dlp import YoutubeDL
+
+list = [
+    "https://www.youtube.com/watch?v=jLsii8s6acM&t=8s"
+]
+
+output_dir = "../data/videos"
+
+def download_yt_video(video_url):
+    yt_opts = {
+        'format': 'best[ext=mp4]',  # Specify the format you want
+        'outtmpl': f'{output_dir}/%(title)s.%(ext)s'
+    }
+
+    with YoutubeDL(yt_opts) as ytdl:
+        ytdl.download(video_url)
+
+
+if __name__ == "__main__":
+    output_dir = "./data/videos"
+    for url in list:
+        download_yt_video(url)
