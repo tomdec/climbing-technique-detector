0c0c89dd15f2a2430bcdcbe4c192c09f2c5186f3
Tom <tom.decoutere@gmail.com>
2025-02-21 11:04:25 +0100

    image dataset generation

--- generate-samples.py
+++ generate-samples.py
@@ -1,6 +1,6 @@
-from src.video.sampling import generate_all_samples
+from src.video.sampling.segments import generate_all_segments
 
 __data_root = "./data"
 
 if __name__ == '__main__':
-    generate_all_samples(__data_root)
+    generate_all_segments(__data_root)
