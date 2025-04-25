3858702b2a9afaf9ea0156b4a16fb79e89434cdc
Tom <tom.decoutere@gmail.com>
2025-04-25 15:34:31 +0200

    final data collection

--- build-database.py
+++ build-database.py
@@ -5,7 +5,8 @@ __list = [
     #"https://www.youtube.com/watch?v=jLsii8s6acM&t=8s",
     #"https://www.youtube.com/watch?v=2LEnGELJxw0",
     #"https://www.youtube.com/watch?v=U0v9xxqEjfY",
-
+    "https://www.youtube.com/watch?v=LOkhYCi-IrE",
+    
     #Lattice Training
     "https://www.youtube.com/watch?v=s-ArwEzr-2M",
     "https://www.youtube.com/watch?v=EBYpXAii9WQ"
