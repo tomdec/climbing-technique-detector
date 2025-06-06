6b36794de0cf216d7c470253bc6c36273080ac25
Tom <tom.decoutere@gmail.com>
2025-06-06 09:50:50 +0200

    add new dnn architectures

--- src/hpe_dnn/model.py
+++ src/hpe_dnn/model.py
@@ -9,8 +9,10 @@ from os import listdir
 from keras._tf_keras.keras.callbacks import ModelCheckpoint, TensorBoard
 from os import makedirs
 from keras._tf_keras.keras.models import load_model
+from typing import Mapping
+from enum import Enum
 
-from common import get_split_limits
+from src.common import get_split_limits
 
 __data_location_from_notebook = "../data/df/hpe-dnn-data.pkl"
 
@@ -148,6 +150,10 @@ def make_input_layer(train: tf.data.Dataset, normalize=True):
     return all_inputs, layers.concatenate(encoded_feature)
 
 def make_hpe_dnn_model(train: tf.data.Dataset, normalize=True, debugging=False) -> Model:
+    """
+        Arch1.
+        Don't change, make new DnnArch and function.
+    """
 
     input_dict, all_features = make_input_layer(train, normalize=normalize)
     intermediate = layers.Dense(256, activation="relu")(all_features)
@@ -168,6 +174,63 @@ def make_hpe_dnn_model(train: tf.data.Dataset, normalize=True, debugging=False) 
         run_eagerly=debugging)
     
     return model
+
+def make_hpe_dnn_model2(train: tf.data.Dataset, normalize=True, debugging=False) -> Model:
+    """
+        Arch2.
+        Don't change, make new DnnArch and function.
+    """
+
+    input_dict, all_features = make_input_layer(train, normalize=normalize)
+    intermediate = layers.Dense(128, activation="relu")(all_features)
+    intermediate = layers.Dropout(0.1)(intermediate)
+    intermediate = layers.Dense(64, activation="relu")(intermediate)
+    intermediate = layers.Dropout(0.1)(intermediate)
+    intermediate = layers.Dense(32, activation="relu")(intermediate)
+    intermediate = layers.Dropout(0.1)(intermediate)
+    output = layers.Dense(7, activation="softmax")(intermediate)
+
+    model = Model(input_dict, output)
+
+    model.compile(optimizer='adam', 
+        loss=losses.BinaryCrossentropy(), 
+        metrics=['accuracy'], 
+        run_eagerly=debugging)
+    
+    return model
+
+def make_hpe_dnn_model3(train: tf.data.Dataset, normalize=True, debugging=False) -> Model:
+    """
+        Arch3.
+        Don't change, make new DnnArch and function.
+    """
+
+    input_dict, all_features = make_input_layer(train, normalize=normalize)
+    intermediate = layers.Dense(64, activation="relu")(all_features)
+    intermediate = layers.Dropout(0.1)(intermediate)
+    intermediate = layers.Dense(32, activation="relu")(intermediate)
+    intermediate = layers.Dropout(0.1)(intermediate)
+    output = layers.Dense(7, activation="softmax")(intermediate)
+
+    model = Model(input_dict, output)
+
+    model.compile(optimizer='adam', 
+        loss=losses.BinaryCrossentropy(), 
+        metrics=['accuracy'], 
+        run_eagerly=debugging)
+    
+    return model
+
+class DnnArch(Enum):
+    ARCH1 = 0
+    ARCH2 = 1
+    ARCH3 = 2
+
+_arch_mapping: Mapping[DnnArch, object] = {
+    DnnArch.ARCH1: make_hpe_dnn_model,
+    DnnArch.ARCH2: make_hpe_dnn_model2,
+    DnnArch.ARCH3: make_hpe_dnn_model3
+}
 
 def plot_model(model: Model):
     utils.plot_model(model, show_shapes=True, show_layer_names=True, rankdir="TB")
@@ -238,3 +301,101 @@ def train_best_model(data_root_path) -> Model:
     train_model(model, train_ds, val_ds, data_root_path)
     
     return model
+
+from os.path import exists
+from typing import Optional
+
+class HpeDnn:
+
+    data_root_path: str
+    name: str
+    dataset_name: str 
+    model: Optional[Model]
+
+    def __init__(self, data_root_path: str, name: str, 
+            dataset_name: str = "techniques"):
+        
+        if (name == ""):
+            raise Exception(f"'{name}' is not a valid name")
+        
+        self.data_root_path = data_root_path
+        self.name = name
+        self.dataset_name = dataset_name
+    
+    def initialize_model(self, arch: DnnArch = DnnArch.ARCH1):
+        if (exists(self.__get_model_dir())):
+            model_path = self.__get_best_model_path()
+            self.__load_model(model_path)
+        else:
+            self.__fresh_model(arch)
+    
+    def train_model(self):
+        if (self.model is None):
+            raise Exception("Cannot train before model is initialized")
+        
+        train_ds = self.__get_data_from_split("train")
+        val_ds = self.__get_data_from_split("val")
+
+        checkpoint_dir = self.__get_checkpoint_dir()
+        log_dir = self.__get_tensorboard_log_dir()
+        makedirs(checkpoint_dir)
+        makedirs(log_dir)
+        
+        checkpoint_path = join(checkpoint_dir, "epoch_{epoch:02d}__val_accuracy_{val_accuracy:.4f}.keras")
+        cp_callback = ModelCheckpoint(checkpoint_path, 
+            save_best_only=True, 
+            save_weights_only=False, 
+            verbose=1,
+            monitor="val_accuracy")
+        
+        tb_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+        self.model.fit(train_ds, epochs=20, validation_data=val_ds, 
+            callbacks=[cp_callback, tb_callback])
+
+    def __get_latest_train_dir(self):
+        model_dir = self.__get_model_dir()
+        if (exists(model_dir)):
+            train_runs = [dir for dir in listdir(model_dir) if "train" in dir]
+            return join(model_dir, f"train{len(train_runs)+1}")
+        else:
+            return join(model_dir, "train1")
+
+    def __get_checkpoint_dir(self):
+        train_dir = self.__get_latest_train_dir()
+        return join(train_dir, "models")
+
+    def __get_tensorboard_log_dir(self):
+        train_dir = self.__get_latest_train_dir()
+        return join(train_dir, "logs")
+
+    def __get_model_dir(self):
+        return join(self.data_root_path, "runs", "hpe_dnn", self.name)
+    
+    def __get_dataset_dir(self):
+        return join(self.data_root_path, "df", self.dataset_name)
+
+    def __get_best_model_path(self):
+        model_dir = self.__get_model_dir()
+        train_list = listdir(model_dir)
+        model_path = join(model_dir, train_list[-1], "models")
+        model_list = listdir(model_path)
+
+        return join(model_path, model_list[-1])
+
+    def __load_model(self, best_model_path):
+        print(f"loading the model '{self.name}' from '{best_model_path}'")
+        self.model = load_model(best_model_path)
+
+    def __fresh_model(self, arch: DnnArch):
+        print(f"loading a fresh model '{self.name}'")
+
+        train_ds = self.__get_data_from_split("train")
+        normalize = True
+        debugging = False
+        model_func = _arch_mapping[arch]
+        self.model = model_func(train_ds, normalize, debugging)
+    
+    def __get_data_from_split(self, split: str) -> tf.data.Dataset:
+        df = read_data(join(self.__get_dataset_dir(), f"{split}.pkl"))
+        return df_to_dataset(df)
