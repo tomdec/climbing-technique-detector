720043d2308c7482f302b41b77b14876d9cc9d27
tomdec <80046425+tomdec@users.noreply.github.com>
2025-07-15 10:08:35 +0200

    Feat/balance on full dataset
    * clean sota code
    
    * inject correctly
    
    * find labels
    
    * init count dict
    
    * balance sota model
    
    * multi run util function
    
    * poc count classes
    
    * balance hpe dnn on full dataset
    
    * remove comment, reset notebook
    
    * add test
    
    * add config

--- src/sota/model.py
+++ src/sota/model.py
@@ -1,47 +1,9 @@
 from ultralytics import YOLO
 from os.path import join, exists
-from os import listdir, rename
+from os import listdir
 from typing import Optional
 
-def get_fresh_model():
-    return YOLO("yolo11m-cls.yaml")
-
-def __get_last_train(train_list):
-    return train_list[-1]
-
-def get_trained_model(data_root_path):
-    model_root_path = join(data_root_path, "runs", "sota")
-    train_list = listdir(model_root_path)
-    newest_train = __get_last_train(train_list)
-    print(f"Starting from best model weights from train run: '{newest_train}'")
-    best_weights_path = join(model_root_path, newest_train, "weights", "best.pt")
-    return YOLO(best_weights_path)
-
-def train_fresh_model(data_root_path):
-    model = get_fresh_model()
-    project_path = join(data_root_path, "runs", "sota")
-    dataset_path = join(data_root_path, "img", "techniques")
-
-    results = model.train(data=dataset_path, 
-        epochs=20, 
-        imgsz=640,
-        project=project_path)
-    print(results)
-
-def train_best_model(data_root_path,
-        optimizer: str = "auto",
-        lr0: float = 0.01):
-    model = get_trained_model(data_root_path)
-    project_path = join(data_root_path, "runs", "sota")
-    dataset_path = join(data_root_path, "img", "techniques")
-    
-    results = model.train(data=dataset_path, 
-                          epochs=20, 
-                          imgsz=640,
-                          project=project_path,
-                          optimizer=optimizer,
-                          lr0=lr0)
-    print(results)
+from src.sota.balancing import WeightedTrainer
 
 class SOTA:
 
@@ -54,7 +16,7 @@ class SOTA:
             dataset_name: str = "techniques"):
     
         if (name == ""):
-            raise Exception(f"'{name}' is not a valid name")
+            raise Exception(f"name cannot be an empty string")
         
         self.data_root_path = data_root_path
         self.name = name
@@ -67,14 +29,26 @@ class SOTA:
         else:
             self.__fresh_model(name)
 
-    def train_model(self, optimizer: str = "auto", lr0: float = 0.01):
+    def execute_train_runs(self, model, runs=1, optimizer: str = "auto", lr0: float = 0.01, 
+            epochs=20, balanced=False):
+        
+        for run in range(runs):
+            print(f"starting run #{run}")
+            self.initialize_model(name=model)
+            self.train_model(optimizer=optimizer, lr0=lr0, epochs=epochs, balanced=balanced)    
+
+    def train_model(self, optimizer: str = "auto", lr0: float = 0.01, epochs=20, 
+            balanced=False):
         if (self.model is None):
             raise Exception("Cannot train before model is initialized")
         
-        project_path = self.__get_project_dir()
+        trainer = WeightedTrainer if balanced else None
+
         dataset_path = self.__get_dataset_dir()
-        results = self.model.train(data=dataset_path, 
-            epochs=20,
+        project_path = self.__get_project_dir()
+        results = self.model.train(trainer=trainer,
+            data=dataset_path, 
+            epochs=epochs,
             imgsz=640,
             project=project_path,
             optimizer=optimizer,
---
+++ test/hpe_dnn/balancing_test.py
@@ -0,0 +1,16 @@
+from pandas import DataFrame
+from numpy import random
+from src.hpe_dnn.balancing import balance_func_factory
+
+def __get_test_df():
+    data = zip([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 2, 2, 3, 3, 3, 4, 4, 4, 4])
+    return DataFrame(data, columns=['feature1', 'technique'])
+
+def test_balance_func_factory():
+    random.seed(10)
+    test_data = __get_test_df().sample(10)
+
+    fut = balance_func_factory(test_data)
+    balanced = test_data.apply(fut, axis=1)
+
+    assert all(balanced['technique'] == [3, 3, 1, 2, 2, 1, 4, 4, 2, 4])
