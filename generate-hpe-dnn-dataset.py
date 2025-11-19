cbe40c5fe53b22d860396ee1b3fe9e442ff70fcc
tom-windows <tom.decoutere@gmail.com>
2025-11-19 16:26:55 +0100

    generate hpe-dnn dataset from images

--- generate-hpe-dnn-dataset.py
+++ generate-hpe-dnn-dataset.py
@@ -1,33 +1,22 @@
-from sys import argv
-
-from src.sampling.dataframe import combine_dataset, generate_hpe_feature_df
-from src.labels import get_dataset_name
-from src.hpe.mp.landmarks import get_feature_labels as get_mp_features
-from src.hpe.mp.evaluate import extract_features as extract_with_mp
-from src.hpe.yolo.landmarks import get_feature_labels as get_yolo_features
-from src.hpe.yolo.evaluate import extract_features as extract_with_yolo
+from argparse import ArgumentParser
 
 __data_root = "data"
 
 if __name__ == '__main__':
+    parser = ArgumentParser(prog="generate-hpe-dnn-dataset",
+        description="Generate HPE landmark dataset based on the image dataset.")
+    parser.add_argument("-c", "--combine", 
+        action="store_true",
+        help="Combine an existing dataset for k-fold validation.")
+    args = parser.parse_args()
 
-    arguments = argv[1:]
-    img_dataset_name = get_dataset_name()
+    from src.sampling.dataframe import combine_dataset, generate_hpe_feature_df
+    from src.labels import get_dataset_name
 
-    if "--yolo" in arguments:
-        features_names = get_yolo_features()
-        evaluate_func = extract_with_yolo
-        df_dataset_name = img_dataset_name + "_yolo"
-    else:
-        features_names = get_mp_features()
-        evaluate_func = extract_with_mp
-        df_dataset_name = img_dataset_name + "_mp"
+    dataset_name = get_dataset_name()
     
-    if "-c" in arguments:
-        combine_dataset(__data_root, df_dataset_name)
+    if args.combine:
+        print(f"Combining {dataset_name} data splits.")
+        combine_dataset(__data_root, dataset_name)
     else:
-        generate_hpe_feature_df(data_path=__data_root, 
-            feature_names=features_names,
-            evaluate_func=evaluate_func,
-            img_dataset_name=img_dataset_name,
-            df_dataset_name=df_dataset_name)
+        generate_hpe_feature_df(__data_root, dataset_name)
