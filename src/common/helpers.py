1c0b4be5d95004bb018ab7158562ad6935c0cdfd
tom-windows <tom.decoutere@gmail.com>
2026-01-16 09:42:12 +0100

    implement data split

--- src/common/helpers.py
+++ src/common/helpers.py
@@ -6,56 +6,70 @@ from pandas import DataFrame, read_pickle
 from cv2 import imread as cv2_imread, cvtColor, COLOR_BGR2RGB
 from cv2.typing import MatLike
 
+
 def get_filename(path: str):
     _, tail = split(path)
     name, _ = splitext(tail)
     return name
 
+
 def get_split_limits(data_split_ratios):
-    '''
+    """
     data_split_ratios = (train, val, test)
     Returns a pair in floats that represent the data split.
-    '''
-    normalized_split_ratios = array(data_split_ratios)/sum(data_split_ratios)
+    """
+    normalized_split_ratios = array(data_split_ratios) / sum(data_split_ratios)
     train_limit = normalized_split_ratios[0]
     val_limit = normalized_split_ratios[0] + normalized_split_ratios[1]
     return train_limit, val_limit
 
+
 def get_runs(root_path: str, run_type: str):
     return [dir for dir in listdir(root_path) if dir.startswith(run_type)]
 
-def safe_index(totals: DataFrame, index: str, fallback = 0) -> int:
+
+def safe_index(totals: DataFrame, index: str, fallback=0) -> int:
     return totals[index] if index in totals.keys() else fallback
+
 
 def __get_next_run(root_path: str, run_type: str):
     if run_type == "":
         raise ArgumentError(run_type, "Cannot be an empty string")
-    
+
     if not exists(root_path):
         return run_type
-    
+
     runs = get_runs(root_path, run_type)
     return f"{run_type}{len(runs)+1}"
 
+
 def __get_current_run(root_path: str, run_type: str):
-    runs = get_runs(root_path, run_type) 
+    runs = get_runs(root_path, run_type)
     return runs[-1]
+
 
 def get_next_train_run(root_path: str):
     return __get_next_run(root_path, "train")
 
+
 def get_current_train_run(root_path: str):
     return __get_current_run(root_path, "train")
+
 
 def get_next_test_run(root_path: str):
     return __get_next_run(root_path, "test")
 
+
 def get_current_test_run(root_path: str):
     return __get_current_run(root_path, "test")
 
+
 def raise_not_implemented_error(class_name, function_name):
-    raise NotImplementedError(f"Invalid use of the class '{class_name}', it needs to implement" +
-        f"the function '{function_name}'.")
+    raise NotImplementedError(
+        f"Invalid use of the class '{class_name}', it needs to implement"
+        + f"the function '{function_name}'."
+    )
+
 
 def read_dataframe(location, verbose=False) -> DataFrame:
     data_frame = read_pickle(location)
@@ -63,14 +77,21 @@ def read_dataframe(location, verbose=False) -> DataFrame:
         print(data_frame.head())
     return data_frame
 
-def save_dataframe(location: str, df: DataFrame):
+
+def ensure_path_exists(location: str):
     path_to_location = dirname(location)
     makedirs(path_to_location, exist_ok=True)
+
+
+def save_dataframe(location: str, df: DataFrame):
+    ensure_path_exists(location=location)
     df.to_pickle(location)
 
+
 def make_file(filepath):
-    with open(filepath, 'w'):
+    with open(filepath, "w"):
         pass
+
 
 def imread(file_path: str, convert_to_rgb: bool = True) -> MatLike:
     image = cv2_imread(file_path)
--- src/sampling/landmarks.py
+++ src/sampling/landmarks.py
@@ -14,8 +14,18 @@ from src.hpe.mp.draw import draw_my_landmarks, draw_features
 from src.hpe.mp.evaluate import predict_landmarks
 from src.hpe.mp.landmarks import get_feature_labels
 from src.hpe.mp.model import build_holistic_model
-from src.labels import get_labels_as_dataframe, get_label_by_frame_num, LabelsCSV
+from src.labels import (
+    get_labels_as_dataframe,
+    get_label_by_frame_num,
+    LabelsCSV,
+    get_labels_from_video,
+)
 from src.video.play.common import play_video
+
+
+def get_landmark_df_path(video_path: str) -> str:
+    return video_path.replace("/videos/", "/df/videos/").replace(".mp4", ".pkl")
+
 
 # VIDEO
 class HpeExtractionContext:
@@ -36,6 +46,7 @@ class HpeExtractionContext:
     def increment_index(self):
         self.current_index += 1
 
+
 def hpe_extractor_mutator(img: MatLike, context: HpeExtractionContext) -> MatLike:
     img_rgb = cvtColor(img, COLOR_BGR2RGB)
     landmarks = predict_landmarks(img_rgb, context.model)
@@ -43,23 +54,25 @@ def hpe_extractor_mutator(img: MatLike, context: HpeExtractionContext) -> MatLik
     context.increment_index()
     return draw_my_landmarks(img, landmarks)
 
+
 def extract_hpe_dataset(video_path: str) -> DataFrame:
-    df_output = video_path.replace("data/videos", "data/df/videos").replace(".mp4", ".pkl")
-    label_path = video_path.replace("data/videos", "data/labels").replace(".mp4", ".csv")
-    
+    df_output = get_landmark_df_path(video_path)
+    label_path = get_labels_from_video(video_path)
+
     labels = get_labels_as_dataframe(label_path)
 
     with build_holistic_model(static_image_model=False) as model:
         context = HpeExtractionContext(model=model, labels=labels)
-        play_video(video_path=video_path,
-            context = context,
-            mutators=[hpe_extractor_mutator])
-        
+        play_video(
+            video_path=video_path, context=context, mutators=[hpe_extractor_mutator]
+        )
+
     columns = ["frame_num", "label", *get_feature_labels()]
     df = DataFrame(data=context.results, columns=columns)
     df.to_pickle(df_output)
 
     return df
+
 
 # SEGMENT
 class SegmentContext:
@@ -82,72 +95,91 @@ class SegmentContext:
         self.frame = self.start_frame
 
     def store_segment(self):
-        segment: DataFrame = self.df.iloc[self.start_frame:self.frame]
+        segment: DataFrame = self.df.iloc[self.start_frame : self.frame]
         segment = segment.reset_index(drop=True)
         path = join("data", "df", "segments", self.label)
         makedirs(path, exist_ok=True)
         segment.to_pickle(join(path, f"{self.name}__{self.start_frame}.pkl"))
 
+
 def ask_input():
-    print("What would you like to do?:",
+    print(
+        "What would you like to do?:",
         "r: replay segment",
         "s: replay at half speed (accumulative)",
         "y: accept landmarks of segment",
         "n: don't use landmarks for training",
         "q: quit iteration",
-        sep='\n')
-    response = input("answer:")    
-    if response in ['r', 's', 'y', 'n', 'q']:
+        sep="\n",
+    )
+    response = input("answer:")
+    if response in ["r", "s", "y", "n", "q"]:
         return response
     else:
         print("incorrect input")
         return ask_input()
 
-def extract_segment_landmarks(segment_path: str, 
-        context: SegmentContext | None = None,
-        playback_speed: float = 1):
-    
+
+def extract_segment_landmarks(
+    segment_path: str, context: SegmentContext | None = None, playback_speed: float = 1
+):
+
     if context is None:
         context = SegmentContext(segment_path)
     else:
         context.reset()
-    
+
     def mutator(img, context: SegmentContext):
         features = context.get_features()
         img = draw_features(img, features)
         context.increment_frame()
         return img
 
-    play_video(segment_path, 
-        context=context, 
-        mutators=[mutator], 
-        playback_speed=playback_speed)
+    play_video(
+        segment_path, context=context, mutators=[mutator], playback_speed=playback_speed
+    )
 
     response = ask_input()
-    if response == 'r':
+    if response == "r":
         return extract_segment_landmarks(segment_path, context, playback_speed)
-    if response == 's':
-        return extract_segment_landmarks(segment_path, context, 0.5*playback_speed)
+    if response == "s":
+        return extract_segment_landmarks(segment_path, context, 0.5 * playback_speed)
     else:
         return context, response
-    
-def remove_non_existing_segments(accepted_segments: List[str], segments: List[str]) -> Tuple[List[str], bool]:
-    result = [accepted_segment for accepted_segment in accepted_segments if accepted_segment in segments]
+
+
+def remove_non_existing_segments(
+    accepted_segments: List[str], segments: List[str]
+) -> Tuple[List[str], bool]:
+    result = [
+        accepted_segment
+        for accepted_segment in accepted_segments
+        if accepted_segment in segments
+    ]
     changed = len(result) != len(accepted_segments)
     if changed:
-        removed = [accepted_segment for accepted_segment in accepted_segments if accepted_segment not in segments]
+        removed = [
+            accepted_segment
+            for accepted_segment in accepted_segments
+            if accepted_segment not in segments
+        ]
         print(f"Removed {len(removed)} segments:")
         print(removed)
     return result, changed
-    
+
+
 def validate_accepted_segments(label: str):
     segments = glob(join("data", "samples", label, "*.*"), recursive=True)
-    evaluated_segments = glob(join("data", "df", "segments", label, "*.pkl"), recursive=True)
+    evaluated_segments = glob(
+        join("data", "df", "segments", label, "*.pkl"), recursive=True
+    )
 
     accepted_segments_path = join("data", "df", "segments", label, "accepted.npy")
     if exists(accepted_segments_path):
         accepted_segments = load(accepted_segments_path)
-        accepted_segments, changed = remove_non_existing_segments(accepted_segments, segments)
+        accepted_segments, changed = remove_non_existing_segments(
+            accepted_segments, segments
+        )
         if changed:
             save(accepted_segments_path, array(accepted_segments))
     else:
@@ -156,7 +188,11 @@ def validate_accepted_segments(label: str):
     if len(segments) == len(evaluated_segments):
         print("Landmarks are extracted for all segments")
     else:
-        print(f"Only extracted landmarks for {len(evaluated_segments)} out of {len(segments)} segments")
+        print(
+            f"Only extracted landmarks for {len(evaluated_segments)} out of {len(segments)} segments"
+        )
 
     percentage = len(accepted_segments) / len(segments)
-    print(f"Accepted the landmarks of {len(accepted_segments)} out of {len(segments)} segments ({percentage:.2%})")
+    print(
+        f"Accepted the landmarks of {len(accepted_segments)} out of {len(segments)} segments ({percentage:.2%})"
+    )
