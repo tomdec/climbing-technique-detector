e241a728aaac879444fa48662de6d8b9b0e18cfb
tom-windows <tom.decoutere@gmail.com>
2026-02-19 09:54:23 +0100

    move playground code to src

--- src/hpe/common/helpers.py
+++ src/hpe/common/helpers.py
@@ -1,4 +1,3 @@
-from cv2.typing import Scalar
 from numpy.linalg import norm
 from numpy.typing import ArrayLike
 from random import choice
@@ -6,22 +5,26 @@ from os import listdir
 from os.path import join
 from typing import List, Tuple
 
+
 def eucl_distance(start: ArrayLike, stop: ArrayLike) -> float:
     return norm(start - stop)
+
 
 def sample(root_dir: str) -> str:
     """Returns a random file name from the root directory"""
     file_name = choice(listdir(root_dir))
     file_path = join(root_dir, file_name)
-    
+
     return file_path
 
+
 def get_label_file(image_path: str) -> str:
-    return image_path.replace('images', 'labels').replace('.jpg', '.txt')
+    return image_path.replace("images", "labels").replace(".jpg", ".txt")
+
 
 def list_image_label_pairs(root_dir: str) -> List[Tuple[str, str]]:
     image_names = listdir(root_dir)
-    
+
     def make_label_pair(image_name):
         image_path = join(root_dir, image_name)
         return (image_path, get_label_file(image_path))
--- src/hpe/common/landmarks.py
+++ src/hpe/common/landmarks.py
@@ -1,10 +1,18 @@
 from numpy import array, ndarray
 from cv2.typing import MatLike
 from typing import List, Dict
+from os import listdir
+from os.path import join
 
-from src.hpe.common.typing import LabelKeyPoint, MyLandmark, PredictedKeyPoint, Visibility
+from src.hpe.common.typing import (
+    LabelKeyPoint,
+    MyLandmark,
+    PredictedKeyPoint,
+    Visibility,
+)
 from src.common.helpers import raise_not_implemented_error
 from src.hpe.common.helpers import eucl_distance
+
 
 class BoundingBox:
 
@@ -28,17 +36,18 @@ class BoundingBox:
 
     def __str__(self):
         return f"{self.as_dict()}"
-    
+
     def as_dict(self) -> dict:
         return {
-            'x': self._x, 
-            'y': self._y, 
-            'x_width': self._x_width, 
-            'y_width': self._y_width
+            "x": self._x,
+            "y": self._y,
+            "x_width": self._x_width,
+            "y_width": self._y_width,
         }
 
     def distance_to(self, point) -> float:
         return eucl_distance(array([self._x, self._y]), point)
+
 
 class KeyPoints:
     _values: List[LabelKeyPoint]
@@ -46,31 +55,32 @@ class KeyPoints:
     def __init__(self, values):
         self._values = list()
         start_index = 0
-        while(len(values) >= start_index + 3):
+        while len(values) >= start_index + 3:
             stop_index = start_index + 3
             self._values.append(LabelKeyPoint(*values[start_index:stop_index]))
             start_index = stop_index
 
     def __getitem__(self, key: MyLandmark | int):
-        if (key is int):
+        if key is int:
             return self._values[key]
         else:
             return self._values[key.value]
-        
+
     def __str__(self):
         return f"[{",\n".join([str(kp) for kp in self._values])}]"
-    
+
     def draw(self, image: MatLike) -> MatLike:
         result = image.copy()
         for index, value in enumerate(self._values):
-            if (value._visibility == Visibility.MISSING):
+            if value._visibility == Visibility.MISSING:
                 continue
             result = value.draw(result, str(index))
         return result
-    
+
+
 class YoloLabels:
 
-    _class_index : int
+    _class_index: int
     _bounding_box: BoundingBox
     _key_points: KeyPoints
 
@@ -83,7 +93,7 @@ class YoloLabels:
     def __str__(self):
         class_str = f"'class': {self._class_index}"
         box_str = f"'bounding_box': {self._bounding_box}"
-        key_points_str =f"'key_points': {self._key_points}"
+        key_points_str = f"'key_points': {self._key_points}"
 
         return f"{{{class_str}, {box_str}, {key_points_str}}}"
 
@@ -95,18 +105,26 @@ class YoloLabels:
         neck = self._key_points[MyLandmark.NECK]
 
         return eucl_distance(head.as_array(), neck.as_array())
-    
+
     def get_keypoint(self, key: MyLandmark) -> LabelKeyPoint:
         return self._key_points[key]
-    
+
     def distance_to(self, point) -> float:
         return self._bounding_box.distance_to(point)
+
+    def count_landmarks(self) -> int:
+        def count_landmark(landmark: MyLandmark) -> int:
+            label = self.get_keypoint(landmark)
+            return 0 if label.is_missing() else 1
+
+        return sum(list(map(count_landmark, MyLandmark)))
+
 
 class PredictedKeyPoints:
 
     def __init__(self):
         pass
-    
+
     def __getitem__(self, index: MyLandmark) -> PredictedKeyPoint:
         """Get landmark prediction for given index.
         Returns an empty landmark when the landmark was not detected.
@@ -123,7 +141,9 @@ class PredictedKeyPoints:
         raise_not_implemented_error(self.__class__.__name__, self.__getitem__.__name__)
 
     def no_person_detected(self):
-        raise_not_implemented_error(self.__class__.__name__, self.no_person_detected.__name__)
+        raise_not_implemented_error(
+            self.__class__.__name__, self.no_person_detected.__name__
+        )
 
     def can_predict(self, landmark: MyLandmark) -> bool:
         """Check if the tool can predict this landmark.
@@ -137,10 +157,11 @@ class PredictedKeyPoints:
         """
         In the absence of a true object to detect landmarks,
         return a mapping of MyLandmark to booleans:\n
-            - true, meaning no prediction (TN), 
+            - true, meaning no prediction (TN),
             - false, meaning an incorrect prediction (FP),
             - None, if mediapipe is not able to predict this landmark.
         """
+
         def no_prediction_for_landmark(landmark: MyLandmark) -> bool | None:
             if not self.can_predict(landmark):
                 return None
@@ -158,13 +179,14 @@ class PredictedKeyPoints:
     def to_array(self) -> ndarray:
         raise_not_implemented_error(self.__class__.__name__, self.to_array.__name__)
 
+
 def build_yolo_labels(file_path: str) -> List[YoloLabels]:
     result = list()
-    
-    with open(file_path, 'r') as file:
+
+    with open(file_path, "r") as file:
         file_text = file.read()
-    
-    if file_text == '':
+
+    if file_text == "":
         return result
 
     file_lines = file_text.split("\n")
@@ -173,18 +195,38 @@ def build_yolo_labels(file_path: str) -> List[YoloLabels]:
 
     return result
 
+
 def get_most_central(label_list: List[YoloLabels]) -> YoloLabels:
     image_center = array([0.5, 0.5])
     result = label_list[0]
     min_distance = label_list[0].distance_to(image_center)
-    
+
     for index in range(1, len(label_list)):
         distance = label_list[index].distance_to(image_center)
         if distance < min_distance:
             min_distance = distance
             result = label_list[index]
-    
+
     return result
+
 
 def get_mylandmark_count():
     return len(MyLandmark)
+
+
+def count_labelled_hpe_landmarks():
+    label_root_dir = "data/hpe/img/test/labels"
+    totals = 0
+
+    for label_name in listdir(label_root_dir):
+        label_path = join(label_root_dir, label_name)
+        df = build_yolo_labels(label_path)
+
+        if len(df) == 0:
+            continue
+        elif len(df) == 1:
+            totals += df[0].count_landmarks()
+        else:
+            totals += get_most_central(df).count_landmarks()
+
+    return totals
