2279657436ca6ea66f4d363b4a460b4cd49cf6ec
tom-windows <tom.decoutere@gmail.com>
2026-03-31 10:16:37 +0200

    add conv_lstm architectures

--- src/conv_lstm/architecture.py
+++ src/conv_lstm/architecture.py
@@ -5,6 +5,7 @@ from keras.api.layers import (
     InputLayer,
     ConvLSTM2D,
     MaxPooling2D,
+    MaxPooling3D,
     Conv2D,
     Dense,
     Reshape,
@@ -20,6 +21,9 @@ from src.sampling.images import FRAME_SIZE
 
 class ConvLstmArch(Enum):
     ARCH1 = 1
+    ARCH2 = 2
+    ARCH3 = 3
+    ARCH4 = 4
 
 
 def __arch1_factory() -> Sequential:
@@ -42,8 +46,69 @@ def __arch1_factory() -> Sequential:
     return model
 
 
+def __arch2_factory() -> Sequential:
+    model = Sequential(
+        [
+            InputLayer(shape=(5, FRAME_SIZE, FRAME_SIZE, 3)),
+            ConvLSTM2D(32, 3, return_sequences=True),
+            MaxPooling3D((1, 2, 2)),
+            ConvLSTM2D(32, 3, return_sequences=False),
+            MaxPooling2D(2),
+            Conv2D(16, 3),
+            MaxPooling2D(2),
+            Conv2D(8, 3),
+            MaxPooling2D(2),
+            Flatten(),
+            Reshape((1, -1)),
+            Dense(get_valid_label_count(), activation="softmax"),
+        ]
+    )
+    return model
+
+
+def __arch3_factory() -> Sequential:
+    model = Sequential(
+        [
+            InputLayer(shape=(5, FRAME_SIZE, FRAME_SIZE, 3)),
+            ConvLSTM2D(64, 3, return_sequences=True),
+            MaxPooling3D((1, 2, 2)),
+            ConvLSTM2D(32, 3, return_sequences=False),
+            MaxPooling2D(2),
+            Conv2D(16, 3),
+            MaxPooling2D(2),
+            Conv2D(8, 3),
+            MaxPooling2D(2),
+            Flatten(),
+            Reshape((1, -1)),
+            Dense(get_valid_label_count(), activation="softmax"),
+        ]
+    )
+    return model
+
+
+def __arch4_factory() -> Sequential:
+    model = Sequential(
+        [
+            InputLayer(shape=(5, FRAME_SIZE, FRAME_SIZE, 3)),
+            ConvLSTM2D(32, 3, return_sequences=True),
+            MaxPooling3D((1, 4, 4)),
+            ConvLSTM2D(16, 3, return_sequences=False),
+            MaxPooling2D(4),
+            Conv2D(8, 3),
+            MaxPooling2D(4),
+            Flatten(),
+            Reshape((1, -1)),
+            Dense(get_valid_label_count(), activation="softmax"),
+        ]
+    )
+    return model
+
+
 __arch_mapping: Mapping[ConvLstmArch, Callable[[], Sequential]] = {
     ConvLstmArch.ARCH1: __arch1_factory,
+    ConvLstmArch.ARCH2: __arch2_factory,
+    ConvLstmArch.ARCH3: __arch3_factory,
+    ConvLstmArch.ARCH4: __arch4_factory,
 }
 
 
